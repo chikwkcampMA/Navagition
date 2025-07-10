8a844768cbd70e47485c4b068a79667be29be0ee
ma2.6chicago <mollygirl1232@gmail.com>
2025-07-10 09:38:55 -0500

    Initial Commit

---
+++ Navagition/NavagitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavagitionApp.swift
+//  Navagition
+//
+//  Created by Scholar on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NavagitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
